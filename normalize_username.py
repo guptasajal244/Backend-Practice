53c074ae447c64f2bf55a6a9540bf57c8a8f4590
Sajal <guptasajal244@gmail.com>
2025-12-21 19:50:36 +0530

    implement username normalization with basic caching

--- normalize_username.py
+++ normalize_username.py
@@ -1,10 +1,13 @@
-def normalize_username(username):
-    username = username.strip()
-    username = username.lower()
+cache = {}
+
+def normalize_username(username: str) -> str | None:
+    if username in cache:
+        return cache[username]
     
-    if len(username) < 3:
-        return None
-    if " " in username:
+    username = username.strip().lower()
+    if len(username) < 3 or " " in username:
         return None
     
+    cache[username] = username
     return username
+
