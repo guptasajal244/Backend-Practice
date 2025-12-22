a09db007e22f32b28bea55fa7bbf0e6d37ed2de1
Sajal <guptasajal244@gmail.com>
2025-12-22 21:34:19 +0530

    refractor: functionalize the program

--- string_length_manual.py
+++ string_length_manual.py
@@ -1,7 +1,8 @@
-a = "My name is sajal gupta"
+def str_length(a):
+    count = 0
+    for i in a:
+        count += 1
+    print(f"Length of string is {count}.")
 
-count = 0
-for i in a:
-    count += 1
-print(f"Length of string is {count}.")
 
+str_length("My name is sajal gupta")
